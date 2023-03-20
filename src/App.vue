634b23d16077e2d2706aa444385f65457b13c752
Iskandar Isman <iskandareizyh12@gmail.com>
2023-03-20 13:33:21 +0800

    add: watch with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
   import Meal from './components/Meal.vue';
-  import { ref, reactive } from "vue";
+  import { ref, reactive, watch } from "vue";
 
   export default {
 
@@ -10,7 +10,18 @@
       //--------------------------------------
       const name = ref("The Ramly Burger")
 
+      const cart = reactive([])
+      // const cart = ref([])
+
       const meal = reactive({ name: 'Hamburger 🍔', price: 5 })
+
+      const meals = reactive([
+        { name: 'Hamburger 🍔', price: 5 },
+        { name: 'Cheeseburger 🧀', price: 2 },
+        { name: 'Hotdog 🌭', price: 10 },
+        { name: 'Fries 🍟', price: 20 },
+      ])
+
       // console.log(meal.name)
       //Always use .value when accessing or mutating the value
       // name.value = 'Hello from the setup function'
@@ -24,10 +35,36 @@
       // }
 
       const placeOrder = () => alert("Your order has been placed!")
-      const addItemToCart = (item) => alert(`One ${item} to the cart!`)
+      const addItemToCart = (item) => cart.push(item)
+      // const addItemToCart = (item) => cart.value.push(item)
 
-      return { name, placeOrder, addItemToCart, meal} // To be access outside setup
+      // Watch reactive object or array will always return a reference to a current value
+      // Not right, because when add to cart, the old value should be not appear
+      // watch(cart.value, (newValue, oldValue) => console.log(newValue, oldValue)) 
+
+      // // This is good
+      // watch(
+      //   [name, () => [...cart]], 
+      //   (newValue, oldValue) => console.log(newValue, oldValue)
+      // ) 
+
+      const removeWatcher = watch([() => [...cart]], (newValue, oldValue) => 
+        alert(newValue.join("\n"))
+      ) 
+
+      // watch(name, (newName, oldName) => console.log(newName, oldName), {
+      //   immediate: true, // The watcher won't wait until the data changes the first time to run and fire immidiately
+      //   // deep: true // Allow to watch deeply nested data in object
+      // })
+
+      return { name, cart, placeOrder, addItemToCart, meal, meals, removeWatcher} // To be access outside setup
     },
+
+    // watch: {
+    //   name(newName, oldName) {
+    //     console.log(newName, oldName)
+    //   }
+    // } Option API
 
   }
 </script>
@@ -38,8 +75,13 @@
 
     <input type="text" v-model="name" />
     <button @click="placeOrder">Place Order</button>
+    <button @click="removeWatcher">Hide Cart Alerts</button>
 
-    <Meal :name="meal.name" :price="meal.price" @addToCart="addItemToCart" />
+    <Meal 
+      v-for="meal in meals" 
+      :name="meal.name" 
+      :price="meal.price" 
+      @addToCart="addItemToCart" />
 
   </div>
 </template>
